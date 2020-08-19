d9fc95728d0db8425b957b6275762dfab8c16c28
niriksha02 <nirikshashree02@gmail.com>
2020-08-20 04:08:40 +0530

    database

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'niriksha',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 18, 2020'
-    },
-    {
-        'author': 'prathiksha',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 19, 2020'
-    }
-]
+
+
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
