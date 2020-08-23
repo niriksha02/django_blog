f9c589fba2510ee4ad5b6530557486ac3a23dfb1
niriksha02 <nirikshashree02@gmail.com>
2020-08-23 22:54:09 +0530

    login and logout

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from .models import Post
 
 
-
-
-
 def home(request):
     context = {
         'posts': Post.objects.all()
@@ -12,6 +9,5 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 
-
 def about(request):
-    return  render(request,'blog/about.html', {'title': 'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
